02a67b44232605971a38e1abc196b951b8f687dd
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2020-04-10 21:30:48 +0400

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  pathPrefix: "/gatsby-react-bootstrap-starter",
+  pathPrefix: "/",
   siteMetadata: {
-    title: `Gatsby React Bootstrap Starter`,
-    description: `A starter that includes react-bootstrap and react-icons, along with SASS compilation.`,
-    author: `Billy Jacoby`,
+    title: `Ashvin Motye`,
+    description: `Portfolio by Ashvin Motye.`,
+    author: `Ashvin Motye`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
