398f658090f0067e1930bf8e5deddcd099c2ecb0
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2020-04-15 21:19:34 +0400

    [TEST] - Check pathname

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,24 +5,27 @@ import SEO from "../components/seo"
 
 import { GiBattleship } from "react-icons/gi"
 
-const NotFoundPage = ({ location }) => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="text-center">
-      <div className="mb-3">
-        <h1 className="mb-2">Currently migrating...</h1>
-        <span class="ship">
-          <GiBattleship />
-        </span>
+const NotFoundPage = ({ location }) => {
+  console.log(location)
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div className="text-center">
+        <div className="mb-3">
+          <h1 className="mb-2">Currently migrating...</h1>
+          <span className="ship">
+            <GiBattleship />
+          </span>
+        </div>
+        <p className="mb-0">Try this</p>
+        <p>
+          <a href={`https://ashvinmotye.github.io${location.pathname}/`}>
+            {`https://ashvinmotye.github.io${location.pathname}/`}
+          </a>
+        </p>
       </div>
-      <p className="mb-0">Try this</p>
-      <p>
-        <a href={`https://ashvinmotye.github.io${location.pathname}/`}>
-          {`https://ashvinmotye.github.io${location.pathname}/`}
-        </a>
-      </p>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default NotFoundPage
