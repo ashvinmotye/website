66d14a6efaaf57c6b24bb6838a3cf4a32e5af823
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2020-04-15 22:42:58 +0400

    Add loader

--- src/components/404/link.js
+++ src/components/404/link.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react"
 
 const Link = ({ location }) => {
   useEffect(() => {
-    document.querySelector(
-      "#redirect"
-    ).href = `https://ashvinmotye.github.io${window.location.pathname}`
+    const link = document.querySelector("#redirect")
+    link.href = `https://ashvinmotye.github.io${window.location.pathname}`
+    link.classList.toggle("d-none")
+
+    document.querySelector(".loading").classList.toggle("d-none")
   })
 
   return (
     <p>
+      <span className="loading">Loading</span>
       <a
         id="redirect"
+        className="d-none"
         href={`https://ashvinmotye.github.io${location.pathname}`}
       >
         {`https://ashvinmotye.github.io${location.pathname}`}
