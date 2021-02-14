cddd5aba1a2bbb2be14119bdb0f02f3ff90053de
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2021-02-14 23:06:07 +0400

    Add logo + projects link

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,19 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 import { Navbar, Nav, Container } from "react-bootstrap"
 
-import logo from "../images/icon.png"
+import logo from "../images/logo-am--light.svg"
 
 const Menu = () => (
   <>
     <Navbar expand="lg">
       <Container>
-        <Nav className="mr-auto sr-only">
+        <Nav className="mr-auto">
           <Link to="/">
             <img src={logo} alt="Home" />
           </Link>
         </Nav>
 
-        <Nav className="ml-auto"></Nav>
+        <Nav className="ml-auto">
+          <a className="menu-link" href="http://ashvinmotye.github.io/projects/">Projects</a>
+        </Nav>
       </Container>
     </Navbar>
   </>
