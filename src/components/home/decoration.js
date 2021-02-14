23d7aa9555c5978b687a6abe9f100535718782aa
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2021-02-14 23:34:19 +0400

    WIP - Add home decoration images

---
+++ src/components/home/decoration.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Decoration = () => (
+  <div className="decoration">
+    <div className="deco deco--1"></div>
+    <div className="deco deco--2"></div>
+  </div>
+)
+
+export default Decoration
